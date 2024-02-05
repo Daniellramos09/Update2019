0e5634b05c6efa92340045a7924a451155aeba66
Daniel Expedito <danielexpeditospr2@gmail.com>
2024-02-05 15:21:37 -0300

    first controller and razor pages view

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,8 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
+            ViewData["Message"] = "Salles Web MVC app from c# course";
+            ViewData["Aluno"] = "Daniel";
             return View();
         }
 
